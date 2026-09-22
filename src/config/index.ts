af522841b9166a26c5a9c04f681d2bddfd61ff25
LuvGaze <2773434682@qq.com>
2026-09-22 20:57:14 +0800

    导入新主题色配置

--- src/config/index.ts
+++ src/config/index.ts
@@ -59,5 +59,5 @@ export { weatherConfig } from "./weatherConfig"; // 天气配置
 // 布局配置
 export { sidebarLayoutConfig } from "./sidebarConfig"; // 侧边栏布局配置
 // 核心配置
-export { siteConfig } from "./siteConfig"; // 站点基础配置
+export { siteConfig, THEME_HUE } from "./siteConfig"; // 站点基础配置与推导色相
 export { sponsorConfig } from "./sponsorConfig"; // 打赏配置
